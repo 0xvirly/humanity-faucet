e615f049d83d401e822dcd0ff900a5cba4e903d1
Virly Airina <164808538+0xvirly@users.noreply.github.com>
2025-02-16 06:45:21 +0700

    Update faucet.js

--- faucet.js
+++ faucet.js
@@ -7,6 +7,10 @@ const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 function timelog() {
   return moment().tz('Asia/Jakarta').format('HH:mm:ss | DD-MM-YYYY');
 }
+
+function appendLog(message) {
+  fs.appendFileSync('log.txt', message + '\n');
+}
 const loading = (message, duration) => {
   return new Promise((resolve) => {
     const symbols = ['|', '/', '-', '\\'];
@@ -46,15 +50,14 @@ async function performFaucet(privateKey) {
 	 }
 	);
 
-    console.log(kleur.green(`[${timelog()}] Get faucet successful for ${address}`));
+    console.log(kleur.green(`[${timelog()}] Get faucet successful for ${walletAddress}`));
+    appendLog(`[${timelog()}] Get faucet successful for ${walletAddress}`);
     return {
       humanityFaucet: humanityFaucet.data,
     };
   } catch (error) {
-    console.error(
-      kleur.yellow(`[${timelog()}] Get faucet failed for ${address || 'unknown address'}: `),
-      error.message
-    );
+    console.error(kleur.yellow(`[${timelog()}] Get faucet failed for ${address || 'unknown address'}: `), error.message);
+    appendLog(`[${timelog()}] Get faucet failed for ${address || 'unknown address'}`);
 	return;
     throw error;
   }
@@ -64,10 +67,11 @@ async function runFaucet() {
     for (const [index, privateKey] of PRIVATE_KEYS.entries()) {
         try {
           await performFaucet(privateKey);
-		  await loading(`Delay 1 minute before next account`, 60000); 
+	  await loading(`Delay 1 minute before next account`, 60000); 
           console.log('');
         } catch (error) {
             console.error(kleur.red(`[${timelog()}] Error processing wallet ${index + 1}: ${error.message}`));
+	    appendLog(`[${timelog()}] Error processing wallet ${index + 1}: ${error.message}`);
         }
     }
 }
